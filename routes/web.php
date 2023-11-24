3deb842de9ba0126aa6fab8ee38fa716bb9429de
Kayke da Penha <kayrodrigues0410@gmail.com>
2023-11-24 18:10:32 -0300

    Rotas com regex

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,16 @@ Route::get('/contato/{nome}', function(string $nome) {
 });
 */
 
+/*
+    Aula sobre parametros opcionais e valores default
+
 Route::get('/contato/{nome}/{assunto?}', function(string $nome, string $assunto = 'valor default') {
     echo "Estamos aqui $nome, falando sobre $assunto";
 });
+*/
+
+Route::get('/contato/{nome}/{categoria_id}', function(string $nome = 'Desconhecido', int $categoria_id = 1) {
+    echo "Estamos aqui: $nome - $categoria_id";
+})
+    ->where('categoria_id', '[0-9]+')
+    ->where('nome', '[a-zA-Z]+');
