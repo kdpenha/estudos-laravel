45b96011f0dad7fc7c290e4d9ca219e32cafe9f8
Kayke da Penha <kayrodrigues0410@gmail.com>
2023-11-24 00:42:08 -0300

    aula sobre parametros opcionais e valores default

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,14 @@ Route::get('/sobrenos', [App\Http\Controllers\SobreNosController::class, 'index'
 
 Route::get('/contato', [App\Http\Controllers\ContatoController::class, 'index']);
 
+/*
+    Aula sobre parametrização dentro das rotas
+
 Route::get('/contato/{nome}', function(string $nome) {
     echo "Estamos aqui $nome";
+});
+*/
+
+Route::get('/contato/{nome}/{assunto?}', function(string $nome, string $assunto = 'valor default') {
+    echo "Estamos aqui $nome, falando sobre $assunto";
 });
