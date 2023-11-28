de998d1295e1e703e4e86cd2a9238828215e866f
Kayke da Penha <kayrodrigues0410@gmail.com>
2023-11-28 00:04:00 -0300

    conteudo dentro de slot do componente e passando a borda por parametro

--- resources/views/site/layouts/_components/form_contato.blade.php
+++ resources/views/site/layouts/_components/form_contato.blade.php
@@ -1,19 +1,20 @@
 <form action="{{route('site.contato')}}" method="post">
     @csrf
-    <input name="nome" type="text" placeholder="Nome" class="borda-preta">
+    <input name="nome" type="text" placeholder="Nome" class="{{$borda}}">
     <br>
-    <input name="telefone" type="text" placeholder="Telefone" class="borda-preta">
+    <input name="telefone" type="text" placeholder="Telefone" class="{{$borda}}">
     <br>
-    <input name="email" type="text" placeholder="E-mail" class="borda-preta">
+    <input name="email" type="text" placeholder="E-mail" class="{{$borda}}">
     <br>
-    <select name="motivo_contato" class="borda-preta">
+    <select name="motivo_contato" class="{{$borda}}">
         <option value="">Qual o motivo do contato?</option>
         <option value="1">Dúvida</option>
         <option value="2">Elogio</option>
         <option value="3">Reclamação</option>
     </select>
     <br>
-    <textarea name="mensagem" class="borda-preta">Preencha aqui a sua mensagem</textarea>
+    <textarea name="mensagem" class="{{$borda}}">Preencha aqui a sua mensagem</textarea>
     <br>
     <button type="submit" class="borda-preta">ENVIAR</button>
+    {{$slot}}
 </form>
