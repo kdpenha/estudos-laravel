9dc38039e7e2a4e1f0db9345d54f0470644d030d
Kayke da Penha <kayrodrigues0410@gmail.com>
2023-12-19 19:40:53 -0300

    fazendo a mesma coisa para a view principal

--- app/Http/Controllers/PrincipalController.php
+++ app/Http/Controllers/PrincipalController.php
@@ -9,7 +9,13 @@ class PrincipalController extends Controller
     //
     public function index(Request $request) {
         $titulo_pagina = 'Principal';
+        
+        $motivo_contatos = [
+            '1' => 'Duvida',
+            '2' => 'Elogio',
+            '3' => 'Reclamação'
+        ];
 
-        return view('site.principal', compact('titulo_pagina'));
+        return view('site.principal', compact('titulo_pagina', 'motivo_contatos'));
     } 
 }
