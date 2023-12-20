81bed95a18a60a3e3f5cfee1016213c1e5001a28
Kayke da Penha <kayrodrigues0410@gmail.com>
2023-12-20 02:54:13 -0300

    adicionando coluna motivo contato id com foreign key na table site contatos

---
+++ database/migrations/2023_12_20_031350_alter_table_site_contatos_add_fk.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('site_contatos', function(Blueprint $table) {
+            $table->unsignedBigInteger('motivo_contato_id');
+        });
+
+        DB::statement('update site_contatos set motivo_contato_id = motivo_contato');
+
+        Schema::table('site_contatos', function(Blueprint $table) {
+            $table->foreign('motivo_contato_id')->references('id')->on('motivo_contatos');
+            $table->dropColumn('motivo_contato');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+        Schema::table('site_contatos', function(Blueprint $table) {
+            $table->dropForeign(['motivo_contato_id']);
+            $table->integer('motivo_contato');
+        });
+
+        DB:statement('update site_contatos motivo_contato = motivo_contato_id');
+
+        Schema::table('site_contatos', function(Blueprint $table) {
+            $table->dropColumn('motivo_contato_id');
+        });
+    }
+};
