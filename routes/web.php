b220b9690c3d9452a85fb58aea3296b8a5c70ba0
Kayke da Penha <kayrodrigues0410@gmail.com>
2024-01-05 19:57:05 -0300

    colocando o bootstrap para o paginator

--- routes/web.php
+++ routes/web.php
@@ -40,6 +40,7 @@ Route::middleware('autenticacao:padrao,visitante')
         
         Route::get('/fornecedor', [App\Http\Controllers\FornecedorController::class, 'index'])->name('app.fornecedor');
         Route::post('/fornecedor/listar', [App\Http\Controllers\FornecedorController::class, 'listar'])->name('app.fornecedor.listar');
+        Route::get('/fornecedor/listar', [App\Http\Controllers\FornecedorController::class, 'listar'])->name('app.fornecedor.listar');
         Route::get('/fornecedor/adicionar', [App\Http\Controllers\FornecedorController::class, 'adicionar'])->name('app.fornecedor.adicionar');
         Route::post('/fornecedor/adicionar', [App\Http\Controllers\FornecedorController::class, 'adicionar'])->name('app.fornecedor.adicionar');
         Route::get('/fornecedor/editar/{id}/{msg?}', [App\Http\Controllers\FornecedorController::class, 'editar'])->name('app.fornecedor.editar');
