f2ad407c6dc6ba57f2fcd0b7e6edb1cad1847f74
Kayke da Penha <kayrodrigues0410@gmail.com>
2024-01-05 20:19:29 -0300

    criando nova rota para exclusao de registro

--- routes/web.php
+++ routes/web.php
@@ -44,6 +44,7 @@ Route::middleware('autenticacao:padrao,visitante')
         Route::get('/fornecedor/adicionar', [App\Http\Controllers\FornecedorController::class, 'adicionar'])->name('app.fornecedor.adicionar');
         Route::post('/fornecedor/adicionar', [App\Http\Controllers\FornecedorController::class, 'adicionar'])->name('app.fornecedor.adicionar');
         Route::get('/fornecedor/editar/{id}/{msg?}', [App\Http\Controllers\FornecedorController::class, 'editar'])->name('app.fornecedor.editar');
+        Route::get('/fornecedor/excluir/{id}', [App\Http\Controllers\FornecedorController::class, 'excluir'])->name('app.fornecedor.excluir');
         
         Route::get('/produto', [App\Http\Controllers\ProdutoController::class, 'index'])->name('app.produto');
 
