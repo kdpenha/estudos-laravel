002e1a562ec84da297d365e7598c818e62ea0f0c
Kayke da Penha <kayrodrigues0410@gmail.com>
2024-02-02 18:07:11 -0300

    recapitulando o eager loading

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use App\Models\ItemDetalhe;
 use App\Models\Fornecedor;
 
@@ -14,12 +16,12 @@ class Item extends Model
     protected $fillable = ['nome', 'descricao', 'peso', 'unidade_id'];
     protected $table = 'produtos';
 
-    public function itemDetalhe()        
+    public function itemDetalhe(): HasOne 
     {
         return $this->hasOne(ItemDetalhe::class, 'produto_id', 'id');
     }
 
-    public function fornecedor()
+    public function fornecedor(): BelongsTo
     {
         return $this->belongsTo(Fornecedor::class, 'fornecedor_id', 'id');
     }
