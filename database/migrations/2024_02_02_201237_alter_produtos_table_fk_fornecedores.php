220d103cd8f8f4583c83b91cd338dceb43a00ed7
Kayke da Penha <kayrodrigues0410@gmail.com>
2024-02-02 17:31:35 -0300

    estabelecendo relacionamento 1 para N de fornecedores e produtos

---
+++ database/migrations/2024_02_02_201237_alter_produtos_table_fk_fornecedores.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //
+        Schema::table('produtos', function(Blueprint $table) {
+
+            $table->unsignedBigInteger('fornecedor_id')->default(8)->after('id');
+            $table->foreign('fornecedor_id')->references('id')->on('fornecedores');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('produtos', function(Blueprint $table) {
+            $table->dropForeign(['fornecedor_id']);
+            $table->dropColumn('fornecedor_id');
+        });
+    }
+};
