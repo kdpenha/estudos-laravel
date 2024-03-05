2a3e9657c95b701fcd355d74d2ed11947a72d76d
Kayke da Penha <kayrodrigues0410@gmail.com>
2024-03-05 18:47:20 -0300

    store e create de pedido-produto

---
+++ resources/views/app/pedido_produto/_components/form_create.blade.php
@@ -0,0 +1,16 @@
+<form action="{{ route('pedido-produto.store', ['pedido' => $pedido])}}" method="POST">
+    @csrf
+
+    <select name="produto_id">
+        <option>-- Selecione um produto --</option>
+
+        @foreach ($produtos as $produto )
+            <option value="{{$produto->id}}" {{ old('produto_id') == $produto->id ? 'selected' : ''  }}>{{$produto->nome}}</option>                
+            @error('produto_id')
+                {{$message}}
+            @enderror
+        @endforeach
+    </select>
+
+    <button type="submit">Cadastrar</button>
+</form>
